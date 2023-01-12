f075bf2cc0562158180e15f6cc4dc566139272cc
kantale <alexandros.kanterakis@gmail.com>
2023-01-12 13:47:18 +0200

    removed translate.R

--- Translation/R/translate.R
+++ Translation/R/translate.R
@@ -1,51 +1,56 @@
 rm(list = ls())
 options(stringsAsFactors=FALSE)
 
-library(doSNOW)
-source("R/match.R")
+# This runs the first time and builds the files in the output directory.
+# Since its had;y ever possible to have to revuild these files I (kantale) have
+# commented it out. To rebuild these files simply remove the if False clause 
+if (F) {
 
-files <- list.files("data/TranslationTables", pattern = "*.table", full.names = T)
-haplotables <- lapply(files, read.table, sep="\t", header=F, comment.char="", quote="")
+  library(doSNOW)
+  source("R/match.R")
 
-refAlleles <- read.table("data/refAlleles.txt", sep="\t", comment.char = "", row.names=1)
+  files <- list.files("data/TranslationTables", pattern = "*.table", full.names = T)
+  haplotables <- lapply(files, read.table, sep="\t", header=F, comment.char="", quote="")
 
-nCores <- 4
+  refAlleles <- read.table("data/refAlleles.txt", sep="\t", comment.char = "", row.names=1)
 
-#creating the cluster
-cl <- makeCluster(nCores)
+  nCores <- 4
 
-#registering the cluster
-registerDoSNOW(cl)
+  #creating the cluster
+  cl <- makeCluster(nCores)
 
-genotypes <- read.table("data/1KG-phase1.txt", sep="\t", header=T, row.names=1)
-print("starting...")
-system.time(
-match1 <- foreach(i= 1:ncol(genotypes), .combine="rbind" ) %dopar% {
-  library(gdata)
-  options(stringsAsFactors=FALSE)
-  match(genotypes,i, haplotables, refAlleles)
-})
+  #registering the cluster
+  registerDoSNOW(cl)
 
-print("done 1092 samples")
+  genotypes <- read.table("data/1KG-phase1.txt", sep="\t", header=T, row.names=1)
+  print("starting...")
+  system.time(
+  match1 <- foreach(i= 1:ncol(genotypes), .combine="rbind" ) %dopar% {
+    library(gdata)
+    options(stringsAsFactors=FALSE)
+    match(genotypes,i, haplotables, refAlleles)
+  })
 
-genotypes <- read.table("data/1KG-phase3.txt", sep="\t", header=T, row.names=1)
+  print("done 1092 samples")
 
-system.time(
-match2 <- foreach(i= 1:ncol(genotypes), .combine="rbind" ) %dopar% {
-  library(gdata)
-  options(stringsAsFactors=FALSE)
-  match(genotypes,i, haplotables, refAlleles)
-})
+  genotypes <- read.table("data/1KG-phase3.txt", sep="\t", header=T, row.names=1)
 
-print("done 2054 samples")
+  system.time(
+  match2 <- foreach(i= 1:ncol(genotypes), .combine="rbind" ) %dopar% {
+    library(gdata)
+    options(stringsAsFactors=FALSE)
+    match(genotypes,i, haplotables, refAlleles)
+  })
 
-#stopping the cluster
-stopCluster(cl)
+  print("done 2054 samples")
 
-fileM <- paste("output/","emodia",0,".match",sep="")
-write.table(match2[match2$sample == "HG00096",], fileM, sep="\t", col.names = T, quote=F, row.names=F)
-fileM <- paste("output/","emodia",1,".match",sep="")
-write.table(match1, fileM, sep="\t", col.names = T, quote=F, row.names=F)
-fileM <- paste("output/","emodia",3,".match",sep="")
-write.table(match2, fileM, sep="\t", col.names = T, quote=F, row.names=F)
-  
+  #stopping the cluster
+  stopCluster(cl)
+
+  fileM <- paste("output/","emodia",0,".match",sep="")
+  write.table(match2[match2$sample == "HG00096",], fileM, sep="\t", col.names = T, quote=F, row.names=F)
+  fileM <- paste("output/","emodia",1,".match",sep="")
+  write.table(match1, fileM, sep="\t", col.names = T, quote=F, row.names=F)
+  fileM <- paste("output/","emodia",3,".match",sep="")
+  write.table(match2, fileM, sep="\t", col.names = T, quote=F, row.names=F)
+}    
